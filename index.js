a8b3e3691773da507896c6fdf5e114948977e47b
SargTeX <martin.christian.bories@gmail.com>
2015-03-20 17:36:18 +0100

    Making ASSTPL asynchronous!

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var regex = require('node-regexp');
+var async = require('async'),
+	regex = require('node-regexp');
 /*
 var reg = regex()
 	.start('[')
@@ -14,16 +15,19 @@ module.exports = {
 	template: function(content, mapping, callback) {
 		var results = content.match(reg);
 		var match;
-		for(var index in results) {
-			var res = results[index];
+		async.each(results, function(res, next) {
 			match = res.substring(1, res.length-1);
 			mapping(match, function(err, result) {
-				if (err) return callback(err);
+				if (err) return next(err);
 
 				//console.log(results[res], match, result);
 				content = content.replace(res, result);
+				next();
 			});
-		}
-		return content;
+		}, function(err) {
+			if (err) return callback(err);
+
+			callback(false, content);
+		});
 	}
 };
